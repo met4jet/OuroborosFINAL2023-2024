acb661a97da363bd52709c5f770b8b0c6286cb43
sophiazhang14 <88739834+sophiazhang14@users.noreply.github.com>
2023-10-06 16:46:37 -0500

    opencv -sophia zhang

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/VisionAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/VisionAuto.java
@@ -5,12 +5,15 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvInternalCamera;
 
 @Autonomous(name="Pixel Detector", group="Auto")
 public class VisionAuto extends LinearOpMode {
 
     OpenCvCamera vision;
+    private PixelDetector detector = new PixelDetector();
+    private String position;
     @Override
     public void runOpMode() throws InterruptedException {
         int cameraMonitorViewId = hardwareMap.appContext
@@ -18,27 +21,27 @@ public class VisionAuto extends LinearOpMode {
                         "id", hardwareMap.appContext.getPackageName());
         vision = OpenCvCameraFactory.getInstance()
                 .createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
-        Detector detector = new Detector(telemetry);
+        vision.openCameraDevice();
         vision.setPipeline(detector);
-        /*vision.openCameraDeviceAsync(
-                () -> vision.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT)
-        );*/
 
-        waitForStart();
-        switch (detector.getLocation()){
-            case FRONT:
-                //...
-                break;
-            case MIDDLE:
-                //...
-                break;
-            case BACK:
-                //...
-                break;
-            case NOT_FOUND:
-                //...
+        vision.startStreaming(320,240, OpenCvCameraRotation.SIDEWAYS_LEFT);
+
+
+        while(!isStarted()){
+            position = detector.position;
+            telemetry.addData("position", position);
         }
-        vision.stopStreaming();
 
+        // code while robot is running
+
+        if(position.equals("LEFT")){
+
+        }
+        else if(position.equals("RIGHT")){
+
+        }
+        else{
+
+        }
     }
 }
