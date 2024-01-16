72b97fc843a905f4eb7e0561db84b0c35f2dd649
KrishSharma68 <113005362+KrishSharma68@users.noreply.github.com>
2024-01-16 15:07:46 -0700

    BIG UPDATE 2: ELECTRIC BOOGALOO [PID, TRAP, HUSKY]
    -Krish hahahahahha

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -65,10 +65,10 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 73.17330064499293; //73.17330064499293
-    public static double MAX_ACCEL = 73.17330064499293; //15.424375250011759
-    public static double MAX_ANG_VEL = Math.toRadians(299.4658071428571); //299.4658071428571
-    public static double MAX_ANG_ACCEL = Math.toRadians(299.4658071428571); //118.482009860873793
+    public static double MAX_VEL = 15.424375250011759; //73.17330064499293
+    public static double MAX_ACCEL = 15.424375250011759; //15.424375250011759
+    public static double MAX_ANG_VEL = Math.toRadians(118.482009860873793); //299.4658071428571
+    public static double MAX_ANG_ACCEL = Math.toRadians(118.482009860873793); //118.482009860873793
 
     /*
      * Adjust the orientations here to match your robot. See the FTC SDK documentation for details.
